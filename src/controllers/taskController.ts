a463f66bdef057457e67bf8cfb17ab722cb64538
mbalindaba01 <mbalindaba01@gmail.com>
2026-05-21 23:29:03 +0200

    added createTask and getTaskById routes and controllers

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import prisma from "../config/prisma"
+import prisma from "../config/prisma.js"
+
 
 export const getAllTasks = async (
     req: Request, 
@@ -10,5 +11,51 @@ export const getAllTasks = async (
         res.status(200).json(tasks);
     }catch (error) {
         res.status(500).json({ error: "Failed to fetch tasks" });
+    }
+}
+
+export const createTask = async (
+    req: Request, 
+    res: Response
+) => {
+    
+    try {
+        const { title, description } = req.body;
+
+    if (typeof title !== "string"|| typeof description !== "string") {
+        return res.status(400).json({ error: "Please enter valid title and description"});
+    }
+
+    if (title.trim() === "" || description.trim() === "") {
+        return res.status(400).json({ error: "Title and description are required" });
+    }
+    
+        const task = await prisma.task.create({
+            data: {
+                title,
+                description
+            }
+        });
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to create task" });
+    }  
+}
+
+export const getTaskById = async (
+    req: Request, 
+    res: Response
+) => {
+    try {
+        const { id } = req.params;
+        const task = await prisma.task.findUnique({
+            where: { id },
+        });
+        if (!task) {
+            return res.status(404).json({ error: "Task not found" });
+        }
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch task" });
     }
 }
--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getAllTasks } from "../controllers/taskController.js";
+import { createTask, getAllTasks, getTaskById } from "../controllers/taskController.js";
 
 const router = Router();
 
 router.get("/", getAllTasks);
+router.post("/", createTask);
+router.get("/:id", getTaskById);
 
 export default router;
