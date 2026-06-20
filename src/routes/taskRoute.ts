32593014d7acb6ee1a0e7f82498cb939298717e2
mbalindaba01 <mbalindaba01@gmail.com>
2026-06-20 17:43:53 +0200

    configured auth middleware and added types to add the user property to express

--- src/routes/taskRoute.ts
+++ src/routes/taskRoute.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { createTask, deleteTask, getAllTasks, getTaskById, searchTasks, updateTask } from "../controllers/taskController.js";
+import auth from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", getAllTasks);
-router.post("/", createTask);
+router.post("/", auth.middleware, createTask);
 router.get("/search", searchTasks);
 router.get("/:id", getTaskById);
-router.put("/:id", updateTask);
-router.delete("/:id", deleteTask);
+router.put("/:id", auth.middleware, updateTask);
+router.delete("/:id", auth.middleware, deleteTask);
 
 
 export default router;
